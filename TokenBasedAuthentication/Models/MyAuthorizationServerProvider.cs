ce6556fd530b1c4bad8e119a479bf698b60905d0
Vinit Bhargava <er.bhargava.vinit@gmail.com>
2020-06-15 09:45:46 +0530

    Custom Token Based Authentication in web API

---
+++ TokenBasedAuthentication/Models/MyAuthorizationServerProvider.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Owin.Security.OAuth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace TokenBasedAuthentication.Models
+{
+    public class MyAuthorizationServerProvider: OAuthAuthorizationServerProvider
+    {
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            string clientId = string.Empty;
+            string clientSecret = string.Empty;
+            // The TryGetBasicCredentials method checks the Authorization header and
+            // Return the ClientId and clientSecret
+            if (!context.TryGetBasicCredentials(out clientId, out clientSecret))
+            {
+                context.SetError("invalid_client", "Client credentials could not be retrieved through the Authorization header.");
+                context.Rejected();
+                return;
+            }
+            //Check the existence of by calling the ValidateClient method
+            ClientMaster client = (new UserMasterRepository()).ValidateClient(clientId, clientSecret);
+            if (client != null)
+            {
+                // Client has been verified.
+                context.OwinContext.Set<ClientMaster>("oauth:client", client);
+                context.Validated(clientId);
+            }
+            else
+            {
+                // Client could not be validated.
+                context.SetError("invalid_client", "Client credentials are invalid.");
+                context.Rejected();
+            }
+            context.Validated();
+        }
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            using (UserMasterRepository _repo = new UserMasterRepository())
+            {
+                var user = _repo.ValidateUser(context.UserName, context.Password);
+                if (user == null)
+                {
+                    context.SetError("invalid_grant", "Provided username and password is incorrect");
+                    return;
+                }
+                var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+                identity.AddClaim(new Claim(ClaimTypes.Role, user.UserRoles));
+                identity.AddClaim(new Claim(ClaimTypes.Name, user.UserName));
+                identity.AddClaim(new Claim("Email", user.UserEmailID));
+                context.Validated(identity);
+            }
+        }
+    }
+}
---
+++ TokenBasedAuthentication/Models/UserMasterRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TokenBasedAuthentication.Models
+{
+    public class UserMasterRepository : IDisposable
+    {
+        WEBAPI_TOKEN_Bases_AuthEntities context = new WEBAPI_TOKEN_Bases_AuthEntities();
+
+        //This method is used to check and validate the user credentials
+        public UserMaster ValidateUser(string username, string password)
+        {
+            return context.UserMasters.FirstOrDefault(user =>
+            user.UserName.Equals(username, StringComparison.OrdinalIgnoreCase)
+            && user.UserPassword == password);
+        }
+
+        //This method is used to check and validate the Client credentials
+        public ClientMaster ValidateClient(string ClientID, string ClientSecret)
+        {
+            return context.ClientMasters.FirstOrDefault(user =>
+             user.ClientId == ClientID
+            && user.ClientSecret == ClientSecret);
+        }
+
+        public void Dispose()
+        {
+            context.Dispose();
+        }
+    }
+}
